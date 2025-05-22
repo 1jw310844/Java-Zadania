458eed51a21d438f1c3d129321c71d5da519f24a
1jw310844 <149474616+1jw310844@users.noreply.github.com>
2025-05-22 21:38:49 +0200

    Add files via upload

---
+++ Snake/src/Snake/Game.java
@@ -0,0 +1,12 @@
+package Snake;
+
+import java.awt.*;
+
+public class Game {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            MainFrame mainFrame = new MainFrame();
+            mainFrame.setVisible(true);
+        });
+    }
+}
